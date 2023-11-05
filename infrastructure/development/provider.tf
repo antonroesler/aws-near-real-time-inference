efb0a6decf698d927b7b4d88a5e2b39d977dd69f
Anton Rösler <anton.roesler@de.bosch.com>
2023-11-05 13:00:17 +0100

    remove backend

--- infrastructure/development/provider.tf
+++ infrastructure/development/provider.tf
@@ -11,11 +11,4 @@ terraform {
       version = "~> 5.23.1"
     }
   }
-
-  # backend "s3" {
-  #   bucket         = "tf-signal-detection-backend"
-  #   key            = "state/terraform.tfstate"
-  #   region         = "eu-central-1"
-  #   dynamodb_table = "tf-signal-detection-lock-db"
-  # }
 }
